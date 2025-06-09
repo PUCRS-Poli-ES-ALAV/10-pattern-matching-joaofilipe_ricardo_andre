4094e3659ec02979e385bf1be0062470556e8673
joaofirocha <joaofi@icloud.com>
2025-06-09 19:56:07 -0300

    commit 2

--- Main.java
+++ Main.java
@@ -1,2 +1,62 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args){
+        PatternMatch p = new PatternMatch();
+
+        System.out.println("Caso 80");
+        String a = "abcdefghijABCDEFGHIJabcdefghijABCDEFGHIJabcdefghijABCDEFGHIJabcdefghijABCDEFGHIJ";
+        String b = "defghijABCD";
+
+        p.patternMatch(a,b);
+        p.search(a,b);
+        p.KMPSearch(a, b);
+
+
+        System.out.println("Caso 1000");
+        StringBuilder sb2 = new StringBuilder();
+        for (int i = 0; i < 500; i++) sb2.append("A");
+        sb2.append("PATTERN_TO_MATCH");
+        for (int i = 0; i < 1480; i++) sb2.append("B");
+        String text2 = sb2.toString();
+
+        String pattern2 = "PATTERN_TO_MATCH";
+
+        p.patternMatch(text2,pattern2);
+        p.search(text2,pattern2);
+        p.KMPSearch(text2, pattern2);
+
+
+        System.out.println("Caso 500000");
+        StringBuilder sb3 = new StringBuilder();
+        for (int i = 0; i < 30500; i++) sb3.append("Lorem ipsum ");
+        sb3.append("SPECIAL_PATTERN_HERE");
+        for (int i = 0; i < 8650; i++) sb3.append(" dolor sit amet");
+        String text3 = sb3.toString();
+
+        String pattern3 = "SPECIAL_PATTERN_HERE";
+
+        p.patternMatch(text3,pattern3);
+        p.search(text3,pattern3);
+        p.KMPSearch(text3, pattern3);
+
+
+        System.out.println("Caso 500000 - not found");
+        String pattern30 = "Not_Found";
+
+        p.patternMatch(text3,pattern30);
+        p.search(text3,pattern30);
+        p.KMPSearch(text3, pattern30);
+
+        System.out.println("Caso 1000000");
+        StringBuilder sb4 = new StringBuilder();
+        for (int i = 0; i < 500000; i++) sb4.append("A");
+        sb4.append("PATTERN_TO_MATCH");
+        for (int i = 0; i < 500000; i++) sb4.append("B");
+        String text4 = sb4.toString();
+
+        String pattern4 = "PATTERN_TO_MATCH";
+        p.patternMatch(text4,pattern4);
+        p.search(text4,pattern4);
+        p.KMPSearch(text4, pattern4);
+
+    }
 }
--- PatternMatch.java
+++ PatternMatch.java
@@ -1,2 +1,190 @@
-package PACKAGE_NAME;public class PatternMatch {
+public class PatternMatch {
+
+    public int patternMatch(String a, String b) {
+        int size = b.length();
+        int iterations = 0;
+        int instructions = 0;
+        long startTime = System.nanoTime();
+
+        System.out.println("");
+        System.out.println("______________________________");
+        System.out.println("Minha Resolucao");
+        System.out.println("String a size: " + a.length());
+        System.out.println("String b size: " + size);
+        System.out.println("");
+
+
+        for (int i = 0; i <= a.length() - size; i++) {
+            iterations++;
+            instructions++;
+            if (a.substring(i, i + size).equals(b)) {
+                instructions++;
+
+                long endTime = System.nanoTime();
+                print(iterations, instructions, endTime - startTime, i);
+
+                return i;
+            }
+            instructions++;
+        }
+
+        instructions++;
+
+        long endTime = System.nanoTime();
+        print(iterations, instructions, endTime - startTime, -1);
+
+        return -1;
+    }
+
+
+
+    public int search(String txt, String pat) {
+        int M = pat.length();
+        int N = txt.length();
+        long patHash = hash(pat, M);
+        int iterations = 0;
+        int instructions = 0;
+        long startTime = System.nanoTime();
+
+        System.out.println("");
+        System.out.println("______________________________");
+        System.out.println("Rabin-Karp");
+        System.out.println("String txt size: " + N);
+        System.out.println("String pat size: " + M);
+        System.out.println("");
+
+        for (int i = 0; i <= N - M; i++) {
+            iterations++;
+            instructions++;
+            long txtHash = hash(txt.substring(i, i + M), M);
+            instructions++;
+            if (patHash == txtHash) {
+                instructions++;
+                if (txt.substring(i, i + M).equals(pat)) {
+                    instructions++;
+
+                    long endTime = System.nanoTime();
+                   print(iterations, instructions, endTime - startTime, i);
+
+                    return i;
+                }
+
+            }
+
+        }
+
+        instructions++;
+
+        long endTime = System.nanoTime();
+        print(iterations, instructions, endTime - startTime, -1);
+
+        return N;
+    }
+
+    private static final int R = 256;
+    private static final int Q = 2147483647;
+    private long hash(String s, int M) {
+        long h = 0;
+        for (int j = 0; j < M; j++)
+            h = (h * R + s.charAt(j)) % Q;
+        return h;
+    }
+
+
+
+
+    void KMPSearch(String txt, String pat) {
+        int M = pat.length();
+        int N = txt.length();
+        int iterations = 0;
+        int instructions = 0;
+        long startTime = System.nanoTime();
+
+        System.out.println("");
+        System.out.println("______________________________");
+        System.out.println("Knuth-Morris-Pratt");
+        System.out.println("String txt size: " + N);
+        System.out.println("String pat size: " + M);
+        System.out.println("");
+
+
+        int lps[] = new int[M];
+        instructions++;
+        computeLPSArray(pat, M, lps);
+        instructions++;
+
+        int i = 0;
+        int j = 0;
+        instructions += 2;
+
+        while (i < N) {
+            iterations++;
+            instructions++;
+
+            if (pat.charAt(j) == txt.charAt(i)) {
+                j++;
+                i++;
+                instructions += 2;
+            }
+
+            if (j == M) {
+                instructions++;
+                long endTime = System.nanoTime();
+                print(iterations, instructions, endTime - startTime, i - j);
+                j = lps[j - 1];
+                instructions++;
+                return ;
+            } else if (i < N && pat.charAt(j) != txt.charAt(i)) {
+                instructions += 2;
+                if (j != 0) {
+                    j = lps[j - 1];
+                    instructions++;
+                } else {
+                    i++;
+                    instructions++;
+                }
+            }
+        }
+
+        long endTime = System.nanoTime();
+        print(iterations, instructions, endTime - startTime, -1);
+    }
+
+        void computeLPSArray(String pat, int M, int lps[]) {
+            // tamanho do maior prefixo sufixo anterior
+            int len = 0;
+            int i = 1;
+            lps[0] = 0; // lps[0] is always 0
+
+            // loop calcula lps[i] for i = 1 to M-1
+            while (i < M) {
+                if (pat.charAt(i) == pat.charAt(len)) {
+                    len++;
+                    lps[i] = len;
+                    i++;
+                } else // (pat[i] != pat[len])
+                {
+                    if (len != 0) {
+                        len = lps[len - 1];
+                    } else // if (len == 0)
+                    {
+                        lps[i] = len;
+                        i++;
+                    }
+                }
+            }
+        }
+
+
+
+    public void print(int iteraction, int instructions, long time, int position) {
+        System.out.println("Iteracoes: " + iteraction);
+        System.out.println("Instrucoes: " + instructions);
+        System.out.println("Execution time: " + time + " ns");
+        if (position == -1)
+            System.out.println("position = not_found");
+        else
+            System.out.println("position = " + position);
+        System.out.println("______________________________");
+    }
 }
